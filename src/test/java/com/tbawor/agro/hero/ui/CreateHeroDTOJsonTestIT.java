c24ef4b121379446c40ab29dce0ee5d1745cd05e
Tomasz Bawor <bawortomasz@gmail.com>
2018-04-09 00:02:48 +0200

    Adding create hero command

---
+++ src/test/java/com/tbawor/agro/hero/ui/CreateHeroDTOJsonTestIT.java
@@ -0,0 +1,59 @@
+package com.tbawor.agro.hero.ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.ObjectContent;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class CreateHeroDTOJsonTestIT {
+
+    @Autowired
+    private JacksonTester<CreateHeroDTO> json;
+
+    @Test
+    public void shouldDeserializeCreateHeroDto() throws IOException {
+        // given
+        String jsonContent = ("{" +
+                "'name': 'TestName'," +
+                "'strength': 1," +
+                "'endurance': 2," +
+                "'defense': 3," +
+                "'agility': 4," +
+                "'dexterity': 5," +
+                "'intelligence': 6," +
+                "'charisma': 7," +
+                "'wisdom': 8," +
+                "'willpower': 9," +
+                "'perception': 10," +
+                "'luck': 11" +
+                "}")
+                .replace('\'','"');
+
+        // when
+        final ObjectContent<CreateHeroDTO> parsedContent = json.parse(jsonContent);
+        CreateHeroDTO dto = parsedContent.getObject();
+
+        // then
+        assertThat(dto.getName()).isEqualTo("TestName");
+        assertThat(dto.getStrength()).isEqualTo(1);
+        assertThat(dto.getEndurance()).isEqualTo(2);
+        assertThat(dto.getDefense()).isEqualTo(3);
+        assertThat(dto.getAgility()).isEqualTo(4);
+        assertThat(dto.getDexterity()).isEqualTo(5);
+        assertThat(dto.getIntelligence()).isEqualTo(6);
+        assertThat(dto.getCharisma()).isEqualTo(7);
+        assertThat(dto.getWisdom()).isEqualTo(8);
+        assertThat(dto.getWillpower()).isEqualTo(9);
+        assertThat(dto.getPerception()).isEqualTo(10);
+        assertThat(dto.getLuck()).isEqualTo(11);
+    }
+}
