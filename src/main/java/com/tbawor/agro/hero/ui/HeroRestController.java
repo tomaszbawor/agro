8066fc0b3a30a1b19e8a3e3296ef5b1d2b92c908
Tomasz Bawor <bawortomasz@gmail.com>
2018-04-09 23:45:57 +0200

    Adding rest mapping for creating hero

--- src/main/java/com/tbawor/agro/hero/ui/HeroRestController.java
+++ src/main/java/com/tbawor/agro/hero/ui/HeroRestController.java
@@ -1,13 +1,29 @@
 package com.tbawor.agro.hero.ui;
 
+import com.tbawor.agro.hero.application.HeroApplicationService;
+import com.tbawor.agro.hero.domain.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/hero")
 public class HeroRestController {
 
+    private final HeroApplicationService service;
+
+    @Autowired
+    public HeroRestController(HeroApplicationService service) {
+        this.service = service;
+    }
+
     @PostMapping
-    public void createHero(){
+    public Integer createHero(@RequestBody CreateHeroDto createHeroDto){
+        //TODO: Should not use Hero from domain, only dtos from application layer
+        final Hero hero = service.createHero(createHeroDto);
+        return hero.getId();
     }
 
 }
