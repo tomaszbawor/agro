da153f3bcf7b35c124e84cb1eeff121cc2547266
Tomasz Bawor <bawortomasz@gmail.com>
2018-04-12 10:49:41 +0200

    Checkstyle fix

--- src/main/java/com/tbawor/agro/hero/application/query/HeroQueryService.java
+++ src/main/java/com/tbawor/agro/hero/application/query/HeroQueryService.java
@@ -2,11 +2,10 @@ package com.tbawor.agro.hero.application.query;
 
 import com.tbawor.agro.hero.domain.Hero;
 import com.tbawor.agro.hero.domain.HeroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class HeroQueryService {
