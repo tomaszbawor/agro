2ab0556c2aab2fae1301a73c202da5c9d117451d
Tomasz Bawor <bawortomasz@gmail.com>
2018-05-05 10:31:55 +0200

    Creating private ctor for constants class

--- src/main/java/com/tbawor/agro/security/config/JwtConstants.java
+++ src/main/java/com/tbawor/agro/security/config/JwtConstants.java
@@ -2,6 +2,10 @@ package com.tbawor.agro.security.config;
 
 class JwtConstants {
 
+    private JwtConstants() {
+        // prevent creating instance
+    }
+
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
     static final String SECRET = "SecretKeyToGenJWTs";
