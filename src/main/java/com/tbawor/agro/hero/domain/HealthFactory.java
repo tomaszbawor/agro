d274db5e48f35c6a3836c28753aa3e5470d9509b
Tomasz Bawor <bawortomasz@gmail.com>
2018-05-07 23:17:17 +0200

    Adding health to hero creation process

---
+++ src/main/java/com/tbawor/agro/hero/domain/HealthFactory.java
@@ -0,0 +1,20 @@
+package com.tbawor.agro.hero.domain;
+
+import java.util.Objects;
+
+class HealthFactory {
+
+    Health createHealthFromStatistics(final Statistics statistics) {
+        Objects.requireNonNull(statistics);
+        final Integer calculatedHealthValue = calculateMaxHealth(statistics);
+        final Health health = new Health();
+        health.setMaxHealth(calculatedHealthValue);
+        health.setHealth(calculatedHealthValue);
+        return health;
+    }
+
+    private int calculateMaxHealth(Statistics statistics) {
+        return statistics.getEndurance() * 2;
+    }
+
+}
